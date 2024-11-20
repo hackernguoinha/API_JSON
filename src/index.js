b3f08937a0fc682550490b1d81cbb8dda1514128
NamLe <namle@ginips.com>
2024-11-20 22:16:05 +0700

    Kết nối đến DB mongodb cloud

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import { engine } from 'express-handlebars';
 import routes from './routes/index.js';
+import mongoose from 'mongoose';
+import * as dotenv from 'dotenv';
+dotenv.config()
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 app.use(express.static('src/public'));
 app.use(express.json());
@@ -11,6 +14,15 @@ app.use(express.json());
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', 'src/views');
+
+//Connect DB
+//console.log(process.env.MONGO_DB)
+mongoose.connect(process.env.MONGO_DB)
+.then(() =>{
+  console.log('Connect DB success')
+}).catch((err) => {
+  console.log(err)
+})
 
 routes(app);
 
